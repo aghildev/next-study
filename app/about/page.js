a03f3b2845d459c9f247fc2b67304776faf995f3
aghildev <aghilpwilson1994@gmail.com>
2023-12-03 09:11:50 +0530

    Link tag integrated for navigation

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Link from 'next/link'
 const About = () => {
   return (
-    <div>About Page</div>
+    <>
+    <div className="text-4xl text-red-500">About Page</div>
+    <Link href = "/">Home Page</Link>
+    </>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
    <>
-   <h1>Home Page</h1>
+   <h1 className='text-3xl'>Home Page</h1>
+   <Link href = "/about" className='text-2xl text-fuchsia-900'>About Page</Link>
    </>
   )
 }
