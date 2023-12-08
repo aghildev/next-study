f643f4506901671a53164b48e862f5a274537452
aghildev <aghilpwilson1994@gmail.com>
2023-12-08 10:52:14 +0530

    tried nav bar in the layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1 className='text-4xl text-cyan-600'>This is the Navbar</h1>
+        {children}
+        <h3>This is a footer</h3>
+        </body>
     </html>
   )
 }
