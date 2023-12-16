039ca8bdc9b35f8fdb30664ef43f9bd1f61e9020
aghildev <aghilpwilson1994@gmail.com>
2023-12-16 08:52:47 +0530

    fetch logic for data in server

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
-const drinksPage = () => {
-  return (
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+const drinksPage = async () => {
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data)
+  return(
     <div>drinksPage</div>
   )
+   
+  
 }
 
 export default drinksPage
