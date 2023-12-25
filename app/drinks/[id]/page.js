e95297cb3d3f508cae88c587ff523ad6f5482b96
aghildev <aghilpwilson1994@gmail.com>
2023-12-25 17:07:39 +0530

    Single page data fetched

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,9 +1,22 @@
 import React from 'react'
+const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=`
 
-const DrinksSinglePage = ({params}) => {
-    console.log(params)
+const getDrinkDetails =async(id)=>{
+    const res = await fetch (`${url}${id}`)
+    if(!res.ok){
+        throw new Error("Something went wrong")
+    }
+    const data = await res.json()
+    return data
+}
+const DrinksSinglePage = async ({params}) => {
+    
+    let data = await getDrinkDetails(params.id)
+    console.log(data)
   return (
-    <div>DrinksSinglePage--{params.id}</div>
+    <div>
+<h1>{data?.drinks[0]?.strDrink}</h1>
+    </div>
   )
 }
 
