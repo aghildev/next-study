8de599395de0347bc65b895b2ac6fd88913191bb
aghildev <aghilpwilson1994@gmail.com>
2023-12-26 08:28:58 +0530

    Static image added

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=`
-
+import drinkImg from "./drink.jpg"
 const getDrinkDetails =async(id)=>{
     const res = await fetch (`${url}${id}`)
     if(!res.ok){
@@ -10,12 +10,13 @@ const getDrinkDetails =async(id)=>{
     return data
 }
 const DrinksSinglePage = async ({params}) => {
-    
+    console.log(drinkImg)
     let data = await getDrinkDetails(params.id)
     console.log(data)
   return (
     <div>
 <h1>{data?.drinks[0]?.strDrink}</h1>
+<img src = {drinkImg.src} alt="drink"/>
     </div>
   )
 }
